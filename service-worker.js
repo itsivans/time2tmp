506bfaa6e111ebf280af23de1c272b27f02b071e
itsivans <itsivans@proton.me>
2025-07-25 10:20:26 +0200

    2

---
+++ service-worker.js
@@ -0,0 +1,45 @@
+
+const CACHE_NAME = 'time-tracker-cache-v1';
+const FILES_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/statistics.html',
+  '/manifest.json',
+  '/app.js',
+  '/app-statistics.js',
+  '/icon-192.png',
+  '/icon-512.png',
+  'https://www.gstatic.com/firebasejs/10.12.0/firebase-app-compat.js',
+  'https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore-compat.js',
+  'https://cdn.jsdelivr.net/npm/chart.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keyList => {
+      return Promise.all(keyList.map(key => {
+        if (key !== CACHE_NAME) {
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
